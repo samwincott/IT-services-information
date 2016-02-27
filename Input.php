fd3636082f0a8dc35d515125b875836617e6ff63
samwincott <wincott97@gmail.com>
2016-02-27 17:59:04 +0000

    input page working a lot better

--- Input.php
+++ Input.php
@@ -12,11 +12,28 @@ if (!isset($_SESSION['username'])) {
 	header("location: Output.php");
 }
 
-		//establishing link to database
+//establishing link to database
 $db = new SQLite3('scripts/testing.db') or die('Unable to open database');
 
+//getting all the services
 $sql = "SELECT * FROM services";
 $result = $db->query($sql);
+
+//filling two arrays with information about statuses
+//first array will be key(status_id) => path_to_logo
+//first array will be key(status_id) => name
+$status_sql = "SELECT * FROM statuses";
+$status_result = $db->query($status_sql);
+$status_logo_array = array();
+while($row = $status_result->fetchArray()){
+    $status_logo_array[$row['id']] = $row['link'];
+}
+$status_name_array = array();
+while($row = $status_result->fetchArray()){
+    $status_name_array[$row['id']] = $row['name'];
+}
+
+// $status_array[$status]
 
 //output page table
 echo "<table>"; 
@@ -32,9 +49,24 @@ while($row = $result->fetchArray()){
     $description     = $row['description'];
     $updated = $row['updated'];
     echo "<tr>
-    		<td>".$service."</td>
-    		<td>".$status."</td>
-    		<td>".$description."</td>
+    		<td><input type='text' value="."$service"."></td>
+    		<td>";
+                //the drop down menu has to be dynamically generated, in the case that 
+                //more statuses are added in the future
+                echo "<select>";
+                for($i = 1; $i <= count($status_name_array); $i++){
+                        //if condition is making sure that the status for the row is pre-selected
+                        if ($status_name_array[$i] == $status_name_array[$status]){
+                            echo "<option value="."$status_name_array[$i]"." selected>"."$status_name_array[$i]"."</option>";
+                        }
+                        else{
+                            echo "<option value="."$status_name_array[$i]".">"."$status_name_array[$i]"."</option>";
+                        }
+        
+                }
+                echo "</select> 
+            </td>
+    		<td><input type='text' value="."$description"."></td>
     		<td>".$updated."</td>
     	</tr>";
     echo "<br>";
--- Output.php
+++ Output.php
@@ -11,8 +11,18 @@
 $db = new SQLite3('scripts/testing.db') or die('Unable to open database');
 
 //determining which services to show
-$sql = "SELECT * FROM services WHERE status <> 1";
-$result = $db->query($sql);
+$service_sql = "SELECT * FROM services WHERE status <> 1";
+$service_result = $db->query($service_sql);
+
+//filling an array with information about statuses
+//array will be key(status_id) => path_to_logo
+$status_sql = "SELECT * FROM statuses WHERE id <> 1";
+$status_result = $db->query($status_sql);
+$status_array = array();
+while($row = $status_result->fetchArray()){
+	$status_array[$row['id']] = $row['link'];
+}
+
 
 //output page table
 echo "<table>"; 
@@ -22,14 +32,14 @@ echo "<tr>
 			<th>Description</th>
 			<th>Updated</th>
 		</tr>";  
-while($row = $result->fetchArray()){
+while($row = $service_result->fetchArray()){
     $service     = $row['name'];
     $status    = $row['status'];
     $description     = $row['description'];
     $updated = $row['updated'];
     echo "<tr>
     		<td>".$service."</td>
-    		<td>".$status."</td>
+    		<td><img src="."$status_array[$status]"."></td>
     		<td>".$description."</td>
     		<td>".$updated."</td>
     	</tr>";
