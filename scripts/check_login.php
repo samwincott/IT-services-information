cc5a08355581ab9ab93480b6017e9089374ef8a5
samwincott <wincott97@gmail.com>
2016-02-27 13:50:25 +0000

    updated to sqlite3

--- scripts/check_login.php
+++ scripts/check_login.php
@@ -1,35 +1,24 @@
 <?php
 
-
-$host = "localhost";
-$db_user = "root";
-$db_pass = "password";
 $user_guess = $_POST['name'];
 $password_guess = $_POST['password'];
-$db_name = "testing";
-$tbl_name = "users";
 
 //establishing link to database
-$link = mysqli_connect($host, $db_user, $db_pass, $db_name);
-
-//checking for valid link to database
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
+$db = new SQLite3('testing.db') or die('Unable to open database');
 
 //prevent sql injection
 $user_guess = stripslashes($user_guess);
 $password_guess = stripslashes($password_guess);
-$user_guess = mysqli_real_escape_string($link, $user_guess);
-$password_guess = mysqli_real_escape_string($link, $password_guess);
-$sql="SELECT * FROM $tbl_name WHERE name='$user_guess' and passhash='$password_guess'";
-$result=mysqli_query($link, $sql);
-$count=mysqli_num_rows($result);
+$user_guess = SQLite3::escapeString($user_guess);
+$password_guess = SQLite3::escapeString($password_guess);
 
-if($count==1){
+//checking if credentials are correct
+$sql="SELECT count(*) FROM users WHERE name='$user_guess' and passhash='$password_guess'";
+$result = $db->querySingle($sql);
+
+//if they are correct, redirect to input page
+//if not, redirect to output page
+if($result == 1){
 	session_start();
 	$_SESSION['username'] = $user_guess;
 	header('Location: ../Input.php');
--- scripts/generate_table.php
+++ scripts/generate_table.php
@@ -7,52 +7,9 @@
 
 <?php
 
-$host = "localhost";
-$db_user = "root";
-$db_pass = "password";
-$db_name = "testing";
-$tbl_statuses = "statuses";
-$tbl_services = "services";
-$edit = False;
-
 //establishing link to database
-$link = mysqli_connect($host, $db_user, $db_pass, $db_name);
-
-//checking for valid link to database
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}	
-
-if (basename($_SERVER['PHP_SELF']) == "Input"){
-	$edit = True;
-}
-
-//determining number of possible statuses
-$possible_statuses = mysqli_query($link, "SELECT * FROM statuses");
-$possible_statuses = mysqli_num_rows($possible_statuses);
-
-//creating an array with the paths of the logo files
-$status_logo_paths = mysqli_query($link, "SELECT link FROM statuses WHERE id <> 1");
-$status_logo_paths = mysqli_fetch_array($status_logo_paths);
-
-//creating an array with the names of statuses
-$status_name = mysqli_query($link, "SELECT name FROM statuses WHERE id <> 1");
-$status_name = mysqli_fetch_array($status_name);
-
-
-
-//creating an associative array linking the status names and paths to logo files
-for ($status=1; $status <= $possible_statuses; $status++){
-    $status_arrary=array($status_name[$status]=>$status_logo_paths);
-}
-
-//determining which services don't have the status "running" and therefore need to be displayed, "running" has the foreign key of 1 
-$rows_to_show = mysqli_query($link, "SELECT * FROM services WHERE status <> 1");
-$rows_to_show = mysqli_num_rows($rows_to_show);
-
+$db = new SQLite3('testing.db') or die('Unable to open database');
+	
 //output page table
 echo "<table>"; 
 echo "<tr>
@@ -62,7 +19,7 @@ echo "<tr>
 			<th>Updated</th>
 		</tr>";
 		//////// this isn't working, probably need to figure it out
-    		for ($row=1; $row <= $rows_to_show; $row++) {  
+    		for ($row=1; $row <= $num_rows_to_show; $row++) {  
     			$service_name = mysqli_fetch_array(mysqli_query($link, "SELECT name FROM services WHERE id = '$row'"));
     			$service_status = mysqli_fetch_array(mysqli_query($link, "SELECT status FROM services WHERE id = '$row'"));
     			$service_description = mysqli_fetch_array(mysqli_query($link, "SELECT description FROM services WHERE id = '$row'"));
@@ -76,15 +33,6 @@ echo "<tr>
         	}  
 echo "</table>";  
 
-//many things to fix here
-//the table is now being dynamically generated and displayed correctly, which is good
-//however, it's only displaying the first n tables, depending on how many, n, tables aren't in the 'runnning' state, there's no selection being done 
-//also the whole using function1(function2($thing)) is probably bad and needs sorting
-//as does having to use $service_*[0]
-
-//also, i'm very wary of lines 33-50 that were added in the last commit
-//it hasn't been tested at all to see if its doing what i want
-//it hasn't given any errors though either so that's a positive sign 
 
 ?>
 
