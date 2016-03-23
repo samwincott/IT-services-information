bc853457a911dbd42c51a3c1428f0390fe125a8e
samwincott <wincott97@gmail.com>
2016-03-23 16:37:16 +0000

    Install script mostly working, cron not adding when run as sudo though

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 wget https://github.com/samwincott/IT-services-information/archive/master.zip 
 unzip master.zip
-cd IT-services-information-master/
+echo -n "Enter name for new directory and press [ENTER]: "
+read name
+mv IT-services-information-master $name
+cd $name
 crontab -l > mycron
 echo "*/5 * * * * cd "$PWD"/scripts/ /usr/bin/php "$PWD"/scripts/update_resolved.php" >> mycron
 crontab mycron
 rm mycron
-chown -R www-data:www-data .
-chmod -r 755 .
+chown -hR www-data .
+chmod -R 755 .
+rm ../install.sh
 rm install.sh
-rm master.zip
+rm ../master.zip
